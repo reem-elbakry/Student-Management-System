1114a9c86c618eaa0dfe84c79dc0b917f0858f6f
RimMousa <reemmousa1995@gmail.com>
2021-01-16 21:17:10 +0200

    StudentFactory & database seed

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            
+                'name' => $this->faker->name,
+                'age' => $this->faker->numberBetween(20,25),
+                'speciality' => $this->faker->word,
+            
+        ];
+    }
+}
